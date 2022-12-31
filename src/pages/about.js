a37245bc2caaa93e3301d6d58c1fa48191d86e45
Maziyar Dowlatabadibazaz <maziyardowlat@gmail.com>
2022-12-31 13:27:47 -0700

    New Changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,27 +5,14 @@ const about_us = new URL("../assets/outside.jpg", import.meta.url)
 const About = () => {
     return(
     <div>
-    <div className="intro"> 
-        <div className = "img_1">
-            <img src={about_us}/>  
+        <div className = "photo">
+            <img className= "image" src={about_us}/>  
         </div>
-        <div className="text">
-            <h1> About Us </h1>
+
+        <div className="card">
+            <div className="card__body">
+            </div>
         </div>
-    </div>
-
-    <div className="team-intro">
-        <h1> Our Journey</h1>
-    </div>
-
-    <div className="team-intro">
-        <h1> Maz</h1>
-    </div>
-
-    <div className="team-intro">
-        <h1> Danny </h1>
-    </div>
-
     </div>
 
     )
--- src/pages/purchase.js
+++ src/pages/purchase.js
@@ -7,7 +7,7 @@ const Purchase = () => {
         <div className="date">
             <img src={coffee_pic}/>  
             <div className="text_1">
-                <h1 > This book will purchasable in 2023 </h1>
+                <h1 > Releasing in 2023. </h1>
             </div>
         </div>
     )
